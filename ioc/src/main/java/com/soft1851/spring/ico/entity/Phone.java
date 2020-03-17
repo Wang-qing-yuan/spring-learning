0df3130afcbf72775f6060f75cc150635ab8097a
Wang-qing-yuan <1950016253@qq.com>
2020-03-17 10:38:46 +0800

    手机

---
+++ ioc/src/main/java/com/soft1851/spring/ico/entity/Phone.java
@@ -0,0 +1,44 @@
+package com.soft1851.spring.ico.entity;
+
+/**
+ * @ClassName Phone
+ * @Description TODO
+ * @Author 田震
+ * @Date 2020/3/12
+ **/
+public class Phone {
+    private String brand;
+    private Double price;
+
+    public Phone() {
+    }
+
+    public Phone(String brand, Double price) {
+        this.brand = brand;
+        this.price = price;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "Phone{" +
+                "brand='" + brand + '\'' +
+                ", price=" + price +
+                '}';
+    }
+}
---
+++ ioc/src/main/java/com/soft1851/spring/ico/entity/Student.java
@@ -0,0 +1,69 @@
+package com.soft1851.spring.ico.entity;
+
+import java.util.List;
+
+/**
+ * @ClassName Student
+ * @Description TODO
+ * @Author wangqingyuan
+ * @Date 2020/3/17 &9:35
+ * @Version 1.0
+ **/
+public class Student {
+    private  Integer id;
+    private  String  name;
+    private List<String> hobbies;
+    private  List<Phone> phones;
+
+    public Student(Integer id, String name, List<String> hobbies, List<Phone> phones) {
+        this.id = id;
+        this.name = name;
+        this.hobbies = hobbies;
+        this.phones = phones;
+    }
+    public  Student(){
+
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<String> getHobbies() {
+        return hobbies;
+    }
+
+    public void setHobbies(List<String> hobbies) {
+        this.hobbies = hobbies;
+    }
+
+    public List<Phone> getPhones() {
+        return phones;
+    }
+
+    public void setPhones(List<Phone> phones) {
+        this.phones = phones;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", hobbies=" + hobbies +
+                ", phones=" + phones +
+                '}';
+    }
+}
